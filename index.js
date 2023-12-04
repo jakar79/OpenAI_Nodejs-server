3b8847b1190d710a6205f4bda99e7a8dbe4e1ec2
JABRI Karim <karim.jabri@gmail.com>
2023-12-04 21:35:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
+
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 
 const app = express();
+
 app.use(express.json());
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+
+//PORT
 
 const port = process.env.PORT || 5000;
 
@@ -17,7 +21,7 @@ app.post("/ask", async (req, res) => {
 
   try {
     if (prompt == null) {
-      throw new Error("Uh oh, no prompt was provided");
+      throw new Error("ooooh!, no prompt was provided");
     }
 
     const response = await openai.createCompletion({
